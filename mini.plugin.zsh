6b581fb3ab200640f57877eb8898bac47bd1723b
koalagang <74791721+koalagang@users.noreply.github.com>
2026-03-14 19:52:23 +0000

    make mini able to source plugins no matter where it is located

--- mini.plugin.zsh
+++ mini.plugin.zsh
@@ -1,10 +1,14 @@
-# populate the start of $ZDOTDIR/mini.zsh with configuration options
+# NOTE: you should populate the start of $ZDOTDIR/mini.zsh with configuration options
 # e.g. for auto-ls, you could add:
 # AUTO_LS_CMD='eza --all --long --group-directories-first --sort=size --binary --colour=always --icons=always'
 source $ZDOTDIR/mini.zsh
 
-# at the end of the file, you should declare the plugins you wish to use inside an array like so:
+# find the directory which this file is located in
+0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
+0="${${(M)0:#/*}:-$PWD/$0}"
+
+# NOTE: at the end of the file, you should declare the plugins you wish to use inside an array like so:
 # mini_zsh_plugins=(auto-ls stash fancy-ctrl-z copy-directory copy-buffer)
 for i in "${mini_zsh_plugins[@]}"; do
-    source "plugins/$i/$i.zsh"
+    source "${0:A:h}/$i.zsh"
 done
